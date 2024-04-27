87c02e8f277108fbda92567e06143591bff973d2
Yusri Amir <yusriamir0@gmail.com>
2024-04-27 09:43:01 +0800

    add onkeydown

--- JavaScript/Lesson 9 - DOM/09-project-amz-calculator/09-project-amz-calculator.js
+++ JavaScript/Lesson 9 - DOM/09-project-amz-calculator/09-project-amz-calculator.js
@@ -1,13 +1,20 @@
 const calculateTotal = () => {
     const inputElement = document.querySelector(".js-cost-input");
-    // * 1) ambil teks dari textbox
-    let cost = inputElement.value;
-    console.log(cost);
+    // * 1) ambil teks untuk input dari textbox
+    let cost = Number(inputElement.value);
+    console.log("ini adalah >>> " + typeof cost);
     // * 2) if cost < $40 add $10
     if (cost < 40) {
-        cost = parseInt(cost) + 10;
-        console.log(typeof cost);
+        cost = cost + 10;
+        console.log(cost);
     }
 
     document.querySelector(".displayTotalCost").innerHTML = `$${cost}`;
+};
+
+// bila tekan enter calculation akan ambil alih
+const handleCostKeyDown = (event) => {
+    if (event.key === "Enter") {
+        calculateTotal();
+    }
 };

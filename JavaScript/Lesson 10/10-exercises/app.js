0b1cc2da5a3fe37ae9825b9d6fb99fb680143d36
Yusri Amir <yusriamir0@gmail.com>
2024-05-05 23:19:21 +0800

    add exercise 10c

---
+++ JavaScript/Lesson 10/10-exercises/app.js
@@ -0,0 +1,7 @@
+// 10c
+// Check if whether there is class list of js-button
+if (document.querySelector(".js-button").classList.contains("js-button")) {
+    console.log("This element contains a js-button");
+} else {
+    console.log("This element does not contain a js-button");
+}
